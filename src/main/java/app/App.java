9b159c1929ac7a1e3a6d5f5d4861aa2e44729f30
Juan Miguel Gutierrez V. <munekoporsiempre2017@gmail.com>
2025-11-15 14:03:59 -0500

    structure javafx

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,7 +1,25 @@
 package app;
 
-public class App {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class App extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(
+                "/view/mainView.fxml")); //Cambiar direccion vista principal
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setTitle("Configuración de Equipos");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
     }
 }
