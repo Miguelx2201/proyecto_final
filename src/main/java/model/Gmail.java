ef00d1beabf7814bc6f8d75249552636b6da85f7
Natalia1208-contr <contreras.m.natalia706@gmail.com>
2025-11-17 13:25:07 -0500

    gmail

--- src/main/java/model/Gmail.java
+++ src/main/java/model/Gmail.java
@@ -1,7 +1,14 @@
 package model;
 
-import jakarta.mail.*;
-import jakarta.mail.internet.*;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Transport;
+import jakarta.mail.Session;
+import jakarta.mail.Authenticator;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
 import java.util.Properties;
 
 public class Gmail {
